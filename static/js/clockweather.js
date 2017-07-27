d64aa79cfe7c6df12b23b088eb3fdf3cd7f9d6a2
szwchao <szwchao@gmail.com>
2017-07-27 12:56:36 +0800

    use heweather API

--- static/js/clockweather.js
+++ static/js/clockweather.js
@@ -1,5 +1,6 @@
-var darksky_token = "yourtoken"
-var baidu_token = "yourtoken"
+var baidu_key = "yourkey"
+var he_key = "yourkey"
+var city = ""
 
 $(function() {
     // clock
@@ -61,143 +62,84 @@ $(function() {
         counter: false
     });
 
-    var url = "http://api.map.baidu.com/location/ip?" + "ak=" + baidu_token + "&coor=bd09ll"
-    url = url + "&callback=?" // 添加callback使getJSON识别为jsonp格式，否则报错
-    $.getJSON(url, function(json) {
-        longitude = json.content.point.x
-        latitude = json.content.point.y
-        setData();
-    });
+    if (city.length == 0) {
+        var url = "http://api.map.baidu.com/location/ip?" + "ak=" + baidu_key + "&coor=bd09ll"
+        url = url + "&callback=?" // 添加callback使getJSON识别为jsonp格式，否则报错
+        $.getJSON(url, function(json) {
+            city = json.content.address_detail.city
+        });
+    }
+    setData();
 });
 
-function setDataByDarkSky() {
-    var url = "https://api.darksky.net/forecast/"+ darksky_token + "/" + latitude + "," + longitude + "?lang=zh&units=ca"
-    url = url + "&callback=?" // 添加callback使getJSON识别为jsonp格式，否则报错
-    $.getJSON(url, function(json) {
+function setData() {
+    var url = "https://free-api.heweather.com/v5/weather?city=" + city + "&key=" + he_key;
+    $.getJSON(url, function(data) {
+        console.log(data);
         // 天气动画
-        var icon = json.currently.icon
+        var result = data.HeWeather5[0]
+
+        // city: "苏州",
+        $("#city").text(result.basic.city);
+        // AQI
+        jg.refresh(result.aqi.city.aqi);
+
+        // 天气动画
+        var code = result.now.cond.code
+        var myDate = new Date();
+        var currentHour = myDate.getHours()
+        var daylight = true;
+        if (currentHour > 17 || currentHour < 6) {
+            daylight = false;
+        } else {
+            daylight = true;
+        }
         var animation = "sunny"
-        if (icon.match("clear") && icon.match("day")) {
-            animation = "sunny"
-        } else if (icon.match("clear") && icon.match("night")) {
-            animation = "starry"
-        } else if (icon.match("cloudy")) {
+        if (code == 100) {
+            if (daylight) {
+                animation = "sunny";
+            } else {
+                animation = "starry";
+            }
+        } else if (101 <= code <= 103) {
             animation = "cloudy"
-        } else if (icon.match("rain")) {
-            animation = "rainy"
-        } else if (icon.match("snow")) {
+        } else if (300 <= code <= 313) {
+            if (310 <= code <= 312) {
+                animation = "stormy"
+            } else {
+                animation = "rainy"
+            }
+        } else if (400 <= code <= 407) {
             animation = "snowy"
-        } else if (icon.match("storm")) {
-            animation = "stormy"
         }
         $("#weather_icon").attr("class", animation);
-    });
-}
 
-function setDataByBaidu() {
-    var url = "http://api.map.baidu.com/telematics/v3/weather?location=" + longitude + "," + latitude + "&output=json&ak=" + baidu_token
-    url = url + "&callback=?" // 添加callback使getJSON识别为jsonp格式，否则报错
-    $.getJSON(url, function(json) {
-        //"pm25": "50",
-        //$("#pm25").text(json.results[0].pm25);
-        jg.refresh(json.results[0].pm25);
-        //"currentCity": "苏州",
-        $("#city").text(json.results[0].currentCity);
+        // 当前
+        var now = result.now;
+        $("#temperature-now").text(now.tmp);
+        $("#temperature-now").append("<sup><small>°C</small> </sup>");
 
-        // 今天
-        var weather_today = json.results[0].weather_data[0];
-        //"date": "周五 01月06日 (实时：10℃)",
-        var temp = weather_today.date.split(" ")[2];
-        var s_i = temp.indexOf("：") + 1;
-        var e_i = temp.indexOf("℃");
-        $("#temperature-now").text(temp.substring(s_i, e_i));
-        $("#temperature-now").append("<sup><small>°C</small> </sup>")
-
-        //所有天气情况（”|”分隔符）：晴|多云|阴|阵雨|雷阵雨|雷阵雨伴有冰雹|雨夹雪|小雨|中雨|大雨|暴雨|大暴雨|特大暴雨|阵雪|小雪|中雪|大雪|暴雪|雾|冻雨|沙尘暴|小雨转中雨|中雨转大雨|大雨转暴雨|暴雨转大暴雨|大暴雨转特大暴雨|小雪转中雪|中雪转大雪|大雪转暴雪|浮尘|扬沙|强沙尘暴|霾
-        var weather_dict = {
-            "晴": "clear-",
-            "多云": "partly-cloudy-",
-            "阴": "cloudy",
-            "雨": "rain",
-            "雪": "snow",
-            "雷阵雨": "thunderstorm",
-            "冻雨":"sleet",
-            "雾":"fog",
-            //"风":"wind",
-        }
-
-        function icon_text(weather) {
-            function getWeatherIcon(w, is_day_night) {
-                var day_night = "";
-                var index = "";
-                if (w.match("晴") || w.match("多云")) {
-                    day_night = is_day_night;
-                } else {
-                    day_night = "";
-                }
-                if (w.match("雪")) {
-                    index = "雪";
-                } else if (w.match("雨")) {
-                    if (w.match("雷阵雨")) {
-                        index = "雷阵雨";
-                    }else if (w.match("冻雨")) {
-                        index = "冻雨";
-                    } else {
-                        index = "雨";
-                    }
-                } else if (w.match("雾")) {
-                    index = "雾";
-                } else {
-                    index = w;
-                }
-                return weather_dict[index] + day_night;
-            }
-            //font-icon
-            //var icon_link_head = '<i class="wi wi-forecast-io-';
-            //var icon_link_tail = '"></i>';
-            //svg animation - high cpu load
-            //var icon_link_head = '<object type="image/svg+xml" data="./static/svg/';
-            //var icon_link_tail = '.svg" width="64" height="64"> </object>';
-            //png image - low cpu load
-            var icon_link_head = '<img src="./static/img/';
-            var icon_link_tail = '.png" height="60" width="88"</img>';
-
-            //"weather": "小雨转中雨",
-            var w = weather.split("转");
-            var i = "";
-            var day_night = "";
-            var index = "";
-            if (w[0]) {
-                i += icon_link_head + getWeatherIcon(w[0], "day") + icon_link_tail;
-            }
-            if (w[1]) {
-                i += icon_link_head + getWeatherIcon(w[1], "night") + icon_link_tail;
-            }
-            return i;
-        }
-
-        function split_temperature(temp) {
-            //"temperature": "12 ~ 10℃"
-            var t = temp.replace("℃", "").split("~");
-            if (t[1]) {
-                return $.trim(t[1]) + "℃ / " + $.trim(t[0]) + "℃";
-            } else {
-                return $.trim(t[0]) + "℃";
-            }
-        }
-
+        var icon_link_head = '<img src="./static/img/';
+        var icon_link_tail = '.png" height="70" width="70"</img>';
+        var forecast = result.daily_forecast;
         for (var i = 0; i < 3; ++i) {
-            var data = json.results[0].weather_data[i];
-            $(".weather" + i).text(data.weather);
-            $(".temp" + i).text(split_temperature(data.temperature));
-            //"wind": "东风3-4级",
-            $(".wind" + i).text(data.wind);
-            $("#icon" + i).html(icon_text(data.weather));
+            if (forecast[i].cond.txt_d != forecast[i].cond.txt_n) {
+                txt = forecast[i].cond.txt_d + "转" + forecast[i].cond.txt_n;
+                icon = icon_link_head + forecast[i].cond.code_d + icon_link_tail;
+                var icon_code_night = forecast[i].cond.code_n;
+                if (icon_code_night == 100 || icon_code_night == 103) {
+                    icon_code_night = icon_code_night + "_night";
+                }
+                icon += icon_link_head + icon_code_night + icon_link_tail;
+            } else {
+                txt = forecast[i].cond.txt_d;
+                icon = icon_link_head + forecast[i].cond.code_d + icon_link_tail;
+            }
+            $(".weather" + i).text(txt);
+            $(".temp" + i).text(forecast[i].tmp.min + "℃ / " + forecast[i].tmp.max + "℃");
+            //"wind": "东风3-4级", TODO 也可能是“东北风微风”，不应该加“级”
+            $(".wind" + i).text(forecast[i].wind.dir + forecast[i].wind.sc + "级");
+            $("#icon" + i).html(icon);
         }
     });
-}
-
-function setData() {
-    setDataByBaidu();
-    setDataByDarkSky();
 }
